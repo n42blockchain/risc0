004b4c02db7eb8f7279a661a9cdce81d2a0f095c
Tim Zerrell <tim.zerrell@risczero.com>
2025-02-14 16:58:05 -0800

    Don't write outputs

--- risc0/bigint2/methods/guest/src/bin/extfield_deg4_mul.rs
+++ risc0/bigint2/methods/guest/src/bin/extfield_deg4_mul.rs
@@ -20,8 +20,6 @@ use risc0_zkvm::guest::env;
 
 #[cfg(any(feature = "num-bigint-dig", feature = "num-bigint"))]
 fn main() {
-    use num_bigint::BigUint;
-
     let lhs = [[0u32; risc0_bigint2::field::FIELD_256_WIDTH_WORDS]; risc0_bigint2::field::EXT_DEGREE_4];
     let rhs = [[0u32; risc0_bigint2::field::FIELD_256_WIDTH_WORDS]; risc0_bigint2::field::EXT_DEGREE_4];
     let monic_irr =[[0u32; risc0_bigint2::field::FIELD_256_WIDTH_WORDS]; risc0_bigint2::field::EXT_DEGREE_4];
@@ -29,13 +27,6 @@ fn main() {
     let mut result = [[0u32; risc0_bigint2::field::FIELD_256_WIDTH_WORDS]; risc0_bigint2::field::EXT_DEGREE_4];
 
     risc0_bigint2::field::extfield_deg4_mul_256(&lhs, &rhs, &monic_irr, &prime, &mut result);
-
-    let result = (BigUint::from_slice(&result[0]),
-                  BigUint::from_slice(&result[1]),
-                  BigUint::from_slice(&result[2]),
-                  BigUint::from_slice(&result[3]));
-
-    env::commit(&result);
 }
 
 #[cfg(not(any(feature = "num-bigint-dig", feature = "num-bigint")))]
